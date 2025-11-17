1cc6ebea2574ff3e8e9c816713ade53103a5858c
harshit gaur <harshigaur737t@gmail.com>
2025-11-18 01:35:29 +0530

    12345

---
+++ hooks/src/hooks/useLocalstorage.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+//local  storage -> store [value ,setValue]
+
+const useLocalstorage = (key, initialVALUE) => {
+    const [storeValue, setStoreValue] = useState(() => {
+        const data = localStorage.getItem('key')
+        return data ? JSON.parse(data) : initialVALUE
+    })
+    useEffect(() => {
+        const saved = localStorage.setItem(key, JSON.stringify(storeValue))
+
+    }, [key, storeValue])
+    return [storeValue, setStoreValue]
+}
+
+export default useLocalstorage
